6a84d5d9d9a99eb0678d1240cb0849caa22c7fdb
shu <free1139@163.com>
2019-09-04 15:09:49 +0800

    fix document

--- errors.go
+++ errors.go
@@ -1,8 +1,8 @@
 //
 // 错误记录器
-// errors
+// error recorder
 //
-// 本设计补充并实现了系统的error接口，
+// 本设计补充并实现了系统的error接口。
 // This design complements and implements the error interface of the go package.
 //
 // 用于发生错误时附带发生错误的原因、位置等信息以便还原现场。
@@ -36,11 +36,11 @@
 //        // errors.ErrNoData == err not necessarily true, so use Equal instead.
 //        if !errors.ErrNoData.Equal(err) {
 //            panic(err)
-//	      }
+//        }
 //
 //        // Deals the same error.
 //        fmt.Println(err)
-//	  }
+//    }
 // }
 //
 package errors
@@ -73,12 +73,7 @@ type Error interface {
 	Equal(err error) bool
 }
 
-//
-// 比较两个错误的值是否相等.
-// 该比较有两个范围，
-// 一个是内存是否相等，常用于同个程序中产生的错误的比较，若内存相等，则两个错误是相等的;
-// 一个是值是否相等，常用于跨程序中产生的错误比较，若不是此接口的Error，则比较Error()的值是否相等；
-// 若属于此Error接口, 则比较Code()的值是否相等，
+// Compare two error is same instance or code is match.
 func Equal(err1 error, err2 error) bool {
 	return equal(err1, err2)
 }
@@ -105,9 +100,7 @@ type errImpl struct {
 	data *ErrData
 }
 
-//
-// 创建一个本包Error接口的实例
-//
+// Make a new error with Error type.
 func New(code string) Error {
 	return &errImpl{
 		&ErrData{
@@ -117,11 +110,7 @@ func New(code string) Error {
 	}
 }
 
-//
-// 解析一个错误文本
-// 通常它是一个本包的Error()序列化数据, 该数据是一个json数据，将直接被序列化为本包的Error类型;
-// 若非本包的接口的序列化结构，将被直接New一个新的Error出来
-//
+// Parse from a Error serial.
 func Parse(src string) Error {
 	if len(src) == 0 {
 		return nil
@@ -129,11 +118,8 @@ func Parse(src string) Error {
 	return parse(src)
 }
 
-//
-// 将一个标准的错误转为本包的Error接口类型
-// 若该错误本已经是本包的Error类型，则直接转为本包的Error并原样返回;
-// 若该错误是非本包的Error类型，则调用error.Error()进行值解析创建一个新的本包Error
-//
+// Parse Error from a error instance.
+// If the error is type of Error, it will be return directly.
 func ParseError(src error) Error {
 	if src == nil {
 		return nil
@@ -144,13 +130,8 @@ func ParseError(src error) Error {
 	return parse(src.Error())
 }
 
-//
-// 给一个错误构建错误定位信息
-// 解析error时等价于ParseError，并在解析出的Error后构建当前置的错误定位信息。
-// 若解析出的是本包的Error类型的实现，将在原实现基础上构建错误定位信息，此时也等价于Error的As方法调用。
-//
-// 返回增加了定位信息的新Error实现, 因为是新实例返回，因此可以支持并发操作
-//
+// Record the reason with as, and return a new error with new stack of reason.
+// It would be safe for concurrency.
 func As(err error, reason ...interface{}) Error {
 	if err == nil {
 		return nil
